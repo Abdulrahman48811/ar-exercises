f18f93dbe64c36a4ba0509b726e4ab7b3bd459f1
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-02-01 17:05:02 -0800

    exercise 7

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {:greater_than_or_equal_to => 0}
 end
-
 burnaby = Store.create(
   name: "Burnaby",
   annual_revenue: 300000,
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {:greater_than_or_equal_to => 40, :less_than_or_equal_to => 200 }
+  validates :store_id, presence: true
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ji", last_name: "lous", hourly_rate: 50)
+@store1.employees.create(first_name: "Shay", last_name: "Ray", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Jim", last_name: "Bob", hourly_rate: 60)
+@store2.employees.create(first_name: "Abdul", last_name: "Mo", hourly_rate: 60)
+@store2.employees.create(first_name: "Duray", last_name: "Adde", hourly_rate: 60)
